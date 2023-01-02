ce1b15fe3beaf6ea7b8efb77de6962fa64147768
systnager <systnager@gmail.com>
2023-01-02 17:59:23 +0200

    add some string values

---
+++ res/string.py
@@ -0,0 +1,6 @@
+strings = {
+    "start_watch_youtube": {"eng": "watch YouTube videos", "ukr": "почати перегляд YouTube відео"},
+    "log_in": {"eng": "log in", "ukr": "увійти"},
+    "settings": {"eng": "settings", "ukr": "налаштування"},
+    "exit": {"eng": "exit", "ukr": "вихід"}
+}
