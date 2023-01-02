556e32a3e7ee546946a657c9f0139e8802908131
systnager <systnager@gmail.com>
2023-01-02 18:00:22 +0200

    create window without some action on buttons (only when press exit window closing)

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import json
+from tkinter import Tk, ttk
+from res.string import strings
+
+
 def main():
-    pass
+    settings_file = open("settings.json")
+    settings = json.load(settings_file)
+    settings_file.close()
+
+    root = Tk()
+    root.geometry("250x75")
+    root.resizable(False, False)
+    frame = ttk.Frame(root)
+    frame.grid()
+
+    ttk.Button(frame, text=strings["start_watch_youtube"][settings["language"]], width=40).grid(column=0, row=0)
+    ttk.Button(frame, text=strings["settings"][settings["language"]], width=40).grid(column=0, row=1)
+    ttk.Button(frame, text=strings["exit"][settings["language"]], width=40, command=root.destroy).grid(column=0, row=2)
+
+    root.mainloop()
 
 
 if __name__ == "__main__":
