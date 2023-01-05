641bae9e7234357cd4358d7ec249195784c2bc28
systnager <systnager@gmail.com>
2023-01-05 22:08:29 +0200

    fix bug and change line wrapping

--- main_window.py
+++ main_window.py
@@ -1,5 +1,7 @@
 import tkinter
 from tkinter import Tk
+
+from wmrfast import WMRFast
 from settings import Settings
 from res.string import strings
 
@@ -7,13 +9,15 @@ from res.string import strings
 class MainWindow:
     def __init__(self):
         self.settings = Settings()
+        self.wmrfast = WMRFast()
 
         self.root = Tk()
         self.root.geometry("250x175")
         self.root.resizable(False, False)
 
         self.start_watch_youtube = tkinter.Button(self.root, text=strings["start_watch_youtube"][self.settings.
-                                                  get_settings()["language"]], width=40, height=2)
+                                                  get_settings()["language"]], width=40, height=2,
+                                                  command=self.wmrfast.start_watch_youtube)
         self.exit = tkinter.Button(self.root, text=strings["exit"][self.settings
                                    .get_settings()["language"]], width=40, height=2, command=self.destroy_window)
         self.open_settings = tkinter.Button(self.root, text=strings["settings"][self.settings.get_settings()[
