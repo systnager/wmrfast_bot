73f94056bdf5f4133a5d763e08f9744388bb426c
systnager <systnager@gmail.com>
2023-01-06 09:58:59 +0200

    add new string and refactor code

--- res/string.py
+++ res/string.py
@@ -1,7 +1,26 @@
 strings = {
-    "start_watch_youtube": {"eng": "Watch YouTube videos", "ukr": "Почати перегляд YouTube відео"},
-    "log_in": {"eng": "Log in", "ukr": "Увійти"},
-    "settings": {"eng": "Settings", "ukr": "Налаштування"},
-    "exit": {"eng": "Exit", "ukr": "Вихід"},
-    "save": {"eng": "Save", "ukr": "Зберегти"}
+    "start_watch_youtube": {
+        "eng": "Watch YouTube videos",
+        "ukr": "Почати перегляд YouTube відео"
+    },
+    "start_watch_youtube_forever": {
+        "eng": "Watch YouTube videos forever",
+        "ukr": "Почати перегляд YouTube відео вічно"
+    },
+    "log_in": {
+        "eng": "Log in",
+        "ukr": "Увійти"
+    },
+    "settings": {
+        "eng": "Settings",
+        "ukr": "Налаштування"
+    },
+    "exit": {
+        "eng": "Exit",
+        "ukr": "Вихід"
+    },
+    "save": {
+        "eng": "Save",
+        "ukr": "Зберегти"
+    }
 }
