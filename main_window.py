f421a50d3f00f250d7b082c74c2bb75abb1fb6a6
systnager <systnager@gmail.com>
2023-01-06 09:59:37 +0200

    add new button for forever watching YouTube videos

--- main_window.py
+++ main_window.py
@@ -12,17 +12,24 @@ class MainWindow:
         self.wmrfast = WMRFast()
 
         self.root = Tk()
-        self.root.geometry("250x175")
+        self.root.geometry("250x225")
         self.root.resizable(False, False)
 
         self.start_watch_youtube = tkinter.Button(self.root, text=strings["start_watch_youtube"][self.settings.
                                                   get_settings()["language"]], width=40, height=2,
                                                   command=self.wmrfast.start_watch_youtube)
+
+        self.start_watch_youtube_forever = tkinter.Button(self.root, text=strings["start_watch_youtube_forever"]
+                                                          [self.settings.get_settings()["language"]],
+                                                          width=40, height=2, command=self._start_watch_youtube_forever)
+
         self.exit = tkinter.Button(self.root, text=strings["exit"][self.settings
                                    .get_settings()["language"]], width=40, height=2, command=self.destroy_window)
+
         self.open_settings = tkinter.Button(self.root, text=strings["settings"][self.settings.get_settings()[
             "language"]], width=40, height=2, command=self.settings.open_edit_settings)
 
+        self.start_watch_youtube_forever.pack(padx=10, pady=5)
         self.start_watch_youtube.pack(padx=10, pady=5)
         self.open_settings.pack(padx=10, pady=5)
         self.exit.pack(padx=10, pady=5)
@@ -32,3 +39,7 @@ class MainWindow:
 
     def destroy_window(self):
         self.root.destroy()
+
+    def _start_watch_youtube_forever(self):
+        while True:
+            self.wmrfast.start_watch_youtube()
