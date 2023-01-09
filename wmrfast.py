49335c23636d6cd2675de73b364d3947cdd4f269
systnager <systnager@gmail.com>
2023-01-09 22:31:00 +0200

    fix bug (maybe :) ) when some error marionette browser do not closed and did big problem with RAM (not closed browser eating CPU and RAM)

--- wmrfast.py
+++ wmrfast.py
@@ -27,62 +27,66 @@ class WMRFast:
     def start_view_website(self):
         try:
             driver, options = self._log_in_on_wmrfast()
-            while True:
-                driver.get("https://wmrfast.com/serfingnew.php")
-                while driver.current_url != "https://wmrfast.com/serfingnew.php":
-                    time.sleep(0.1)
-                website_list = driver.find_elements(By.CLASS_NAME, "no_active_link")
-                if len(website_list) == 0:
-                    driver.quit()
-                for i in website_list:
-                    a = i.find_elements(By.CLASS_NAME, "serf_hash")[0]
-                    price_span, time_span = i.find_elements(By.CLASS_NAME, "clickprice")
-                    earned_money = float(price_span.get_attribute('innerHTML'))
-                    time_sleep = float(time_span.get_attribute('innerHTML').split()[0])
-                    a.click()
-                    driver.switch_to.window(driver.window_handles[1])
+            driver.get("https://wmrfast.com/serfingnew.php")
+            while driver.current_url != "https://wmrfast.com/serfingnew.php":
+                time.sleep(0.1)
+            website_list = driver.find_elements(By.CLASS_NAME, "no_active_link")
+            if len(website_list) == 0:
+                driver.quit()
+            for i in website_list:
+                a = i.find_elements(By.CLASS_NAME, "serf_hash")[0]
+                price_span, time_span = i.find_elements(By.CLASS_NAME, "clickprice")
+                earned_money = float(price_span.get_attribute('innerHTML'))
+                time_sleep = float(time_span.get_attribute('innerHTML').split()[0])
+                a.click()
+                driver.switch_to.window(driver.window_handles[1])
 
-                    time.sleep(time_sleep + 3)
-                    driver.close()
-                    driver.switch_to.window(driver.window_handles[0])
-                    time.sleep(3)
-                    self.total_earned_money += earned_money
-                    print(f"{datetime.datetime.now()} earned for session: {earned_money}, total: {self.total_earned_money} in view site")
-        except:
+                time.sleep(time_sleep + 3)
+                driver.close()
+                driver.switch_to.window(driver.window_handles[0])
+                time.sleep(3)
+                self.total_earned_money += earned_money
+                print(f"{datetime.datetime.now()} earned for session: {earned_money}, total: {self.total_earned_money} in view site")
+        except Exception as e:
+            print(f"\n\n\n{datetime.datetime.now()}\nWARNING, {e}\n\n\n\n")
+            print(f"{datetime.datetime.now()} end view site")
+        finally:
             driver.quit()
 
 
     def start_watch_youtube(self):
         try:
             driver, options = self._log_in_on_wmrfast()
-            while True:
-                driver.get("https://wmrfast.com/serfing_ytn.php")
-                while driver.current_url != "https://wmrfast.com/serfing_ytn.php":
+            driver.get("https://wmrfast.com/serfing_ytn.php")
+            while driver.current_url != "https://wmrfast.com/serfing_ytn.php":
+                time.sleep(0.1)
+            video_list = driver.find_elements(By.CLASS_NAME, "sforms")
+            if len(video_list) == 0:
+                driver.quit()
+            for i in video_list:
+                a = i.find_elements(By.CLASS_NAME, "serf_hash")[0]
+                price_span, time_span = i.find_elements(By.CLASS_NAME, "clickprice")
+                earned_money = float(price_span.get_attribute('innerHTML'))
+                time_sleep = float(time_span.get_attribute('innerHTML').split()[0])
+                a.click()
+                while len(driver.window_handles) < 2:
                     time.sleep(0.1)
-                video_list = driver.find_elements(By.CLASS_NAME, "sforms")
-                if len(video_list) == 0:
-                    driver.quit()
-                for i in video_list:
-                        a = i.find_elements(By.CLASS_NAME, "serf_hash")[0]
-                        price_span, time_span = i.find_elements(By.CLASS_NAME, "clickprice")
-                        earned_money = float(price_span.get_attribute('innerHTML'))
-                        time_sleep = float(time_span.get_attribute('innerHTML').split()[0])
-                        a.click()
-                        while len(driver.window_handles) < 2:
-                            time.sleep(0.1)
-                        driver.switch_to.window(driver.window_handles[1])
+                driver.switch_to.window(driver.window_handles[1])
 
 
-                        #driver.find_element(By.XPATH, "/html/body/table/tbody/tr/iframe").click()
-                        time.sleep(time_sleep + 2.5)
-                        driver.execute_script("check()")
-                        time.sleep(0.5)
-                        driver.close()
-                        driver.switch_to.window(driver.window_handles[0])
+                #driver.find_element(By.XPATH, "/html/body/table/tbody/tr/iframe").click()
+                time.sleep(time_sleep + 2.5)
+                driver.execute_script("check()")
+                time.sleep(0.5)
+                driver.close()
+                driver.switch_to.window(driver.window_handles[0])
 
-                        self.total_earned_money += earned_money
-                        print(f"{datetime.datetime.now()} earned for session: {earned_money}, total: {self.total_earned_money} in youtube video")
-        except:
+                self.total_earned_money += earned_money
+                print(f"{datetime.datetime.now()} earned for session: {earned_money}, total: {self.total_earned_money} in youtube video")
+        except Exception as e:
+            print(f"\n\n\n{datetime.datetime.now()}\nWARNING, {e}\n\n\n\n")
+            print(f"{datetime.datetime.now()} end watch youtube")
+        finally:
             driver.quit()
 
     def _log_in_on_wmrfast(self):
