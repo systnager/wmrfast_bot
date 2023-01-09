9bf14b8986fdeac0f953143fa14a7c26998f3ed0
systnager <systnager@gmail.com>
2023-01-09 23:14:35 +0200

    add new strings

--- res/string.py
+++ res/string.py
@@ -22,5 +22,29 @@ strings = {
     "save": {
         "eng": "Save",
         "ukr": "Зберегти"
+    },
+    "cookies_find": {
+        "eng": "Cookies find",
+        "ukr": "Кукі знайдено"
+    },
+    "cookies_not_find": {
+        "eng": "Cookies not find",
+        "ukr": "Кукі не знайдено"
+    },
+    "start_log_in": {
+        "eng": "Start log in",
+        "ukr": "Розпочинається авторизація"
+    },
+    "finish_log_in": {
+        "eng": "Finish log in",
+        "ukr": "Авторизація завершена"
+    },
+    "earned": {
+        "eng": "earned",
+        "ukr": "зароблено"
+    },
+    "total": {
+        "eng": "total",
+        "ukr": "всього"
     }
 }
