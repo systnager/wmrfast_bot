5741d82a13e9c4f50c8f0d93a0eed261346ad0c4
systnager <systnager@gmail.com>
2023-07-01 09:53:38 +0300

    change delay

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ def main():
     driver = wmr_fast.log_in()
     while True:
         driver = wmr_fast.start_watch_youtube(driver)
-        time_sleep = random.randint(0, 60)
+        time_sleep = random.randint(0, 15)
         print(f'{datetime.datetime.now()} sleep {time_sleep} seconds')
         time.sleep(time_sleep)
 
         driver = wmr_fast.start_view_website(driver)
-        time_sleep = random.randint(0, 300)
+        time_sleep = random.randint(0, 200)
         print(f'{datetime.datetime.now()} sleep {time_sleep} seconds')
         time.sleep(time_sleep)
 
