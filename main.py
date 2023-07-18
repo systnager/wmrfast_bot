33ef3a85444adcd0779ca179fb69cd3516c1b36d
systn <systnager@gmail.com>
2023-07-18 17:45:33 +0300

    Refactor and optimize code

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
 import datetime
 import random
+import pickle
 import time
 
 from settings import Settings
 from wmrfast import WMRFast
+from res.string import strings
 
 
 def main():
-    settings = Settings()
-    wmr_fast = WMRFast(settings)
+    _settings = Settings()
+    settings = _settings.get_settings()
+    lan = settings['language']
+    wmr_fast = WMRFast(_settings)
     driver = wmr_fast.log_in()
+    print(f'{datetime.datetime.now()} sleep {5} seconds')
+    time.sleep(5)
+    is_video_tasks_available = True
+    is_website_tasks_available = True
     while True:
-        driver = wmr_fast.start_watch_youtube(driver)
-        time_sleep = random.randint(0, 15)
-        print(f'{datetime.datetime.now()} sleep {time_sleep} seconds')
-        time.sleep(time_sleep)
-
-        driver = wmr_fast.start_view_website(driver)
-        time_sleep = random.randint(0, 200)
-        print(f'{datetime.datetime.now()} sleep {time_sleep} seconds')
-        time.sleep(time_sleep)
+        try:
+            if is_website_tasks_available or is_video_tasks_available:
+                if is_video_tasks_available:
+                    is_video_tasks_available = wmr_fast.watch_videos(driver)['is_tasks_available']
+                if is_website_tasks_available:
+                    is_website_tasks_available = wmr_fast.view_websites(driver)['is_tasks_available']
+            else:
+                print(f"{datetime.datetime.now()} {strings['nothing_watch_or_view'][lan]}")
+                is_video_tasks_available = True
+                is_website_tasks_available = True
+                time.sleep(3600)
+        except KeyboardInterrupt:
+            if input('PRESS ENTER TO CONTINUE OR ENTER STOP TO CLOSE').lower() == 'stop':
+                quit()
+            else:
+                continue
+            
 
 
 if __name__ == "__main__":
--- wmrfast.py
+++ wmrfast.py
@@ -24,119 +24,128 @@ class WMRFast:
         self.settings = settings.get_settings()
         self.lan = self.settings['language']
 
-    def start_view_website(self, driver):
+    def view_websites(self, driver):
         print(f"{datetime.datetime.now()} " +
               f"{strings['view_web'][self.lan]}"
               )
         driver.get("https://wmrfast.com/serfingnew.php")
-
-        website_list = driver.find_elements(By.CLASS_NAME, "no_active_link")
-        if len(website_list) == 0:
-            print(f"{datetime.datetime.now()} {strings['nothing_watch_or_view'][self.lan]}")
-            time.sleep(1800)
-            return driver
-
+        print(f'{datetime.datetime.now()} sleep {5} seconds')
+        time.sleep(5)
         if _is_captcha_available(driver):
             input(f'\n\n\n{bcolors.WARNING}WARNING, COMPLETE THE CAPTCHA AND PRESS ENTER{bcolors.ENDC}\n\n\n')
-        for i in website_list:
-            try:
-                a = i.find_element(By.CLASS_NAME, "serf_hash")
-                price_span, time_span = i.find_elements(By.CLASS_NAME, "clickprice")
-                earned_money = float(price_span.get_attribute('innerHTML'))
-                time_sleep = int(time_span.get_attribute('innerHTML').split()[0]) + 3
-                if 't.me' in a.get_attribute('href'):
-                    continue
-                else:
-                    a.click()
-            except Exception as e:
-                print(f"{bcolors.WARNING}{e}{bcolors.ENDC}")
-                continue
 
-            for j in range(5):
+        website_list = driver.find_elements(By.CLASS_NAME, "no_active_link")
+        is_tasks_available = True
+        if len(website_list) > 0:
+            for i in website_list:
+                try:
+                    a = i.find_element(By.CLASS_NAME, "serf_hash")
+                    price_span, time_span = i.find_elements(By.CLASS_NAME, "clickprice")
+                    earned_money = float(price_span.get_attribute('innerHTML'))
+                    time_sleep = int(time_span.get_attribute('innerHTML').split()[0]) + 5
+                    if 't.me' in a.get_attribute('href'):
+                        continue
+                    else:
+                        a.click()
+                except Exception as e:
+                    print(f"{bcolors.WARNING}{e}{bcolors.ENDC}")
+                    continue
+
+                for j in range(5):
+                    if len(driver.window_handles) < 2:
+                        time.sleep(1)
+                        continue
+                    else:
+                        break
+
                 if len(driver.window_handles) < 2:
-                    time.sleep(1)
                     continue
-                else:
-                    break
 
-            if len(driver.window_handles) < 2:
-                continue
+                driver.switch_to.window(driver.window_handles[1])
+                time.sleep(time_sleep)
+                self.total_earned_money += earned_money
+                print(
+                    f"{bcolors.OKGREEN}{datetime.datetime.now()} " +
+                    f"{strings['earned'][self.lan]}: " +
+                    f"{round(earned_money, 5)}, {strings['total'][self.lan]}: " +
+                    f"{round(self.total_earned_money, 5)}{bcolors.ENDC}"
+                )
+                for handle in driver.window_handles[1:]:
+                    driver.switch_to.window(handle)
+                    driver.close()
 
-            driver.switch_to.window(driver.window_handles[1])
-            time.sleep(time_sleep)
-            self.total_earned_money += earned_money
-            print(
-                f"{bcolors.OKGREEN}{datetime.datetime.now()} " +
-                f"{strings['earned'][self.lan]}: " +
-                f"{round(earned_money, 5)}, {strings['total'][self.lan]}: " +
-                f"{round(self.total_earned_money, 5)}{bcolors.ENDC}"
-            )
-            for handle in driver.window_handles[1:]:
-                driver.switch_to.window(handle)
-                driver.close()
+                driver.switch_to.window(driver.window_handles[0])
+                time.sleep(5)
+        else:
+            is_tasks_available = False
 
-            driver.switch_to.window(driver.window_handles[0])
+        return {
+        'is_tasks_available': is_tasks_available,
+        }
 
-        return driver
-
-    def start_watch_youtube(self, driver):
+    def watch_videos(self, driver):
         print(f"{datetime.datetime.now()} " +
               f"{strings['watch_videos'][self.lan]}"
               )
 
         driver.get("https://wmrfast.com/serfing_ytn.php")
-        video_list = driver.find_elements(By.CLASS_NAME, "sforms")
-        if len(video_list) == 0:
-            print(f"{datetime.datetime.now()} {strings['nothing_watch_or_view'][self.lan]}")
-            time.sleep(1800)
-            return driver
-
         if _is_captcha_available(driver):
             input(f'\n\n\n{bcolors.WARNING}WARNING, COMPLETE THE CAPTCHA AND PRESS ENTER{bcolors.ENDC}\n\n\n')
-        for i in video_list:
-            try:
-                a = i.find_elements(By.CLASS_NAME, "serf_hash")[0]
-                price_span, time_span = i.find_elements(By.CLASS_NAME, "clickprice")
-                earned_money = float(price_span.get_attribute('innerHTML'))
-                time_sleep = int(time_span.get_attribute('innerHTML').split()[0])
-                a.click()
-            except Exception as e:
-                print(f"{bcolors.WARNING}{datetime.datetime.now()}{e}{bcolors.ENDC}")
-                continue
-
-            for j in range(5):
-                if len(driver.window_handles) < 2:
-                    time.sleep(1)
+        print(f'{datetime.datetime.now()} sleep {5} seconds')
+        time.sleep(5)
+        video_list = driver.find_elements(By.CLASS_NAME, "sforms")
+        is_tasks_available = True
+        if len(video_list) > 0:
+            for i in video_list:
+                try:
+                    a = i.find_elements(By.CLASS_NAME, "serf_hash")[0]
+                    price_span, time_span = i.find_elements(By.CLASS_NAME, "clickprice")
+                    earned_money = float(price_span.get_attribute('innerHTML'))
+                    time_sleep = int(time_span.get_attribute('innerHTML').split()[0])
+                    a.click()
+                except Exception as e:
+                    print(f"{bcolors.WARNING}{datetime.datetime.now()}{e}{bcolors.ENDC}")
                     continue
-                else:
-                    break
 
-            if len(driver.window_handles) < 2:
-                continue
+                for j in range(5):
+                    if len(driver.window_handles) < 2:
+                        time.sleep(1)
+                        continue
+                    else:
+                        break
 
-            driver.switch_to.window(driver.window_handles[1])
-            time.sleep(time_sleep)
-            try:
-                driver.execute_script("check()")
-            except Exception as e:
-                print(f"{bcolors.WARNING}{datetime.datetime.now()}{e}{bcolors.ENDC}")
-                continue
-            time.sleep(1.5)
+                if len(driver.window_handles) < 2:
+                    continue
 
-            self.total_earned_money += earned_money
-            print(
-                f"{bcolors.OKGREEN}{datetime.datetime.now()} " +
-                f"{strings['earned'][self.lan]}: " +
-                f"{round(earned_money, 5)}, {strings['total'][self.lan]}: " +
-                f"{round(self.total_earned_money, 5)}{bcolors.ENDC}"
-            )
-            for handle in driver.window_handles[1:]:
-                driver.switch_to.window(handle)
-                driver.close()
+                driver.switch_to.window(driver.window_handles[1])
+                time.sleep(time_sleep)
+                time.sleep(3)
+                try:
+                    driver.execute_script("check()")
+                    time.sleep(3)
+                except Exception as e:
+                    print(f"{bcolors.WARNING}{datetime.datetime.now()}{e}{bcolors.ENDC}")
+                    continue
 
-            driver.switch_to.window(driver.window_handles[0])
+                self.total_earned_money += earned_money
+                print(
+                    f"{bcolors.OKGREEN}{datetime.datetime.now()} " +
+                    f"{strings['earned'][self.lan]}: " +
+                    f"{round(earned_money, 5)}, {strings['total'][self.lan]}: " +
+                    f"{round(self.total_earned_money, 5)}{bcolors.ENDC}"
+                )
+                for handle in driver.window_handles[1:]:
+                    driver.switch_to.window(handle)
+                    driver.close()
 
-        return driver
+                driver.switch_to.window(driver.window_handles[0])
+                time.sleep(1.5)
+        else:
+            is_tasks_available = False
+
+        return {
+        'is_tasks_available': is_tasks_available,
+        }
 
     def log_in(self):
         print(f"{datetime.datetime.now()} {strings['start_log_in'][self.lan]}")
@@ -165,8 +174,9 @@ class WMRFast:
             driver.find_element(By.ID, "vhusername").send_keys(login)
             driver.find_element(By.ID, "vhpass").send_keys(password)
             del auth_data, login, password
+            input(f"{datetime.datetime.now()} Press ENTER after log in")
 
-        pickle.dump(driver.get_cookies(), open("cookies", "wb"))
+            pickle.dump(driver.get_cookies(), open("cookies", "wb"))
         print(f"{datetime.datetime.now()} {strings['finish_log_in'][self.lan]}")
 
         return driver
